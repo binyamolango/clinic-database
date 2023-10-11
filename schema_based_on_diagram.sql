6f90b627439e882be1a710d2871de0c255e892c0
Binyam Yohannes <125895809+binyamolango@users.noreply.github.com>
2023-10-11 12:18:59 +0300

    Add invoice item and treatment tables in schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -33,3 +33,40 @@ CREATE TABLE invoices (
 
 CREATE INDEX idx_medical_history_id ON invoices (medical_history_id);
 
+CREATE TABLE treatments (
+    id int GENERATED ALWAYS AS IDENTITY,
+    type varchar(255),
+    name varchar(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE invoice_items (
+    id int GENERATED ALWAYS AS IDENTITY,
+    unit_price decimal,
+    quantity int,
+    total_price decimal,
+    invoice_id int,
+    treatment_id int,
+    FOREIGN KEY (invoice_id)
+    REFERENCES invoices(id),
+    FOREIGN KEY (treatment_id)
+    REFERENCES treatments (id),
+    PRIMARY KEY (id)
+);
+
+CREATE INDEX idx_invoice_id ON invoice_items (invoice_id);
+CREATE INDEX idx_treatment_id ON invoice_items (treatment_id);
+
+CREATE TABLE treatments_history (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    medical_history_id INT,
+    treatment_id INT,
+    FOREIGN KEY (medical_history_id)
+    REFERENCES medical_histories(id),
+    FOREIGN KEY (treatment_id)
+    REFERENCES treatments(id),
+    PRIMARY KEY (id)
+);
+
+CREATE INDEX idx_medical_history_id ON treatments_history (medical_history_id);
+CREATE INDEX idx_treatment_id ON treatments_history (treatment_id);
